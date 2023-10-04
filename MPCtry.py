77f36afb813761b6841bc18060be29814b860350
prakrutk <prakroot@gmail.com>
2023-10-04 12:24:08 +0530

    MPC prep

--- MPCtry.py
+++ MPCtry.py
@@ -1,61 +1,71 @@
 import pybullet as p
 import numpy as np
 import time
+import cvxopt
 
-# Step 1: Setup PyBullet Environment
-physicsClient = p.connect(p.GUI)
-p.setGravity(0, 0, -9.81)
-p.setTimeStep(1.0 / 240.0)
+class MPC:
+    def __init__(self, robot_id, goal_state, current_state, horizon, dt):
+        self.robot_id = robot_id
+        self.goal_state = goal_state
+        self.current_state = current_state
+        self.horizon = horizon
+        self.dt = dt
 
-# Load or create the robot model in the PyBullet environment
-# robot_id 
+    
+    physicsClient = p.connect(p.GUI)
+    p.setGravity(0, 0, -9.81)
+    p.setTimeStep(1.0 / 240.0)
 
-# Step 2: Define the MPC Controller
-def mpc_controller(current_state, goal_state):
-    # Implement your MPC controller here
-    # Generate control inputs based on current state and goal
+    p.loadURDF("plane.urdf")
 
-# Step 3: Define Robot's Dynamics
-# Define the robot's motion equations, constraints, and physics
+    robot_id = p.loadURDF("racecar/racecar_differential.urdf",[-4,4,0])  #, [0,0,2],useFixedBase=True)
+    for i in range(p.getNumJoints(robot_id)):
+        print(p.getJointInfo(robot_id, i))
+    for wheel in range(p.getNumJoints(robot_id)):
+        p.setJointMotorControl2(robot_id, wheel, p.VELOCITY_CONTROL, targetVelocity=0, force=0)
+        p.getJointInfo(robot_id, wheel)
 
-# Step 4: Define the Cost Function
-def cost_function(state_error, control_input):
-    # Implement your cost function here
-    # Consider state error, control effort, and other factors
+    # Step 2: Define the MPC Controller
+    def mpc_controller(self):
 
-# Step 5: MPC Optimization
-def mpc_optimization():
-    # Implement the MPC optimization algorithm here
-    # Solve for control inputs over a finite time horizon
+        # implement MPC controller here
+        # Generate control inputs based on current state and goal
 
-# Step 6: Simulation Loop
-while True:
-    # Measure the current state of the robot
-    current_state = p.getRobotState(robot_id)  # Replace with the appropriate function
+    # Step 3: Define Robot's Dynamics
+    def robot_dynamics():
 
-    # Define the desired goal state
-    goal_state = [desired_position, desired_orientation]  # Customize as needed
 
-    # Use MPC controller to generate control inputs
-    control_input = mpc_controller(current_state, goal_state)
+    # Define the robot's motion equations, constraints, and physics
 
-    # Apply control inputs to the robot in PyBullet
-    p.setJointMotorControlArray(robot_id, jointIndices, controlMode=p.VELOCITY_CONTROL, targetVelocities=control_input)
+    # Step 4: Define the Cost Function
+    def cost_function(state_error, control_input):
+        # Implement your cost function here
+        # Consider state error, control effort, and other factors
 
-    # Step the simulation forward in time
-    p.stepSimulation()
+    # Step 5: MPC Optimization
+    def mpc_optimization(self):
+        # Implement the MPC optimization algorithm here
+        # Solve for control inputs over a finite time horizon
 
-    # Repeat or exit based on termination conditions
+    # Step 6: Simulation Loop
+        while True:
+            current_state = p.getRobotState(self.robot_id)  # Replace with the appropriate function
+            goal_state = [desired_position, desired_orientation]  # Customize as needed
+            control_input = self.mpc_controller(current_state, goal_state)
+            p.setJointMotorControlArray(robot_id, jointIndices, controlMode=p.VELOCITY_CONTROL, targetVelocities=control_input)
+            p.stepSimulation()
 
-# Step 7: Visualization (Implement as needed)
-# Visualize robot trajectory, control inputs, and simulation
+        # Repeat or exit based on termination conditions
 
-# Step 8: Parameter Tuning (Tune parameters for your specific robot and task)
+    # Step 7: Visualization (Implement as needed)
+    # Visualize robot trajectory, control inputs, and simulation
 
-# Step 9: Safety Mechanisms (Implement collision handling and safety measures)
+    # Step 8: Parameter Tuning (Tune parameters for your specific robot and task)
 
-# Step 10: Testing and Validation
-# Test the MPC controller under various scenarios and validate its performance
+    # Step 9: Safety Mechanisms (Implement collision handling and safety measures)
 
-# Close the PyBullet environment when done
-p.disconnect()
+    # Step 10: Testing and Validation
+    # Test the MPC controller under various scenarios and validate its performance
+
+    # Close the PyBullet environment when done
+    p.disconnect()
