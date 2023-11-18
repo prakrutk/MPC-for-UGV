e26d8e2dc6bb18f25376f27fc2a1af39d577a631
prakrutk <prakroot@gmail.com>
2023-11-18 14:16:52 +0530

    Remove-husky

--- ROS-Gazebo/husky_ws/build/husky_simulator/colcon_command_prefix_build.sh
+++
@@ -1,4 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/prakrutk/Downloads/FOR_Project/ROS-Gazebo/husky_ws/install/husky_description/share/husky_description/package.sh"
-. "/home/prakrutk/Downloads/FOR_Project/ROS-Gazebo/husky_ws/install/husky_control/share/husky_control/package.sh"
-. "/home/prakrutk/Downloads/FOR_Project/ROS-Gazebo/husky_ws/install/husky_gazebo/share/husky_gazebo/package.sh"
--- ROS-Gazebo/husky_ws/build/husky_viz/colcon_command_prefix_build.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/prakrutk/Downloads/FOR_Project/ROS-Gazebo/husky_ws/install/husky_description/share/husky_description/package.sh"
